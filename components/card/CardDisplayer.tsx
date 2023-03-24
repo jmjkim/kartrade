22ce0784f4317305f80e2c939eba27db1d01849e
jmjkmbm116p <jmjkmbm116p@jkm116p.local>
2023-03-23 23:00:23 -0700

    Changed CardDisplayer component type

--- components/card/CardDisplayer.tsx
+++ components/card/CardDisplayer.tsx
@@ -9,60 +9,64 @@ import {
   Heading,
   Divider,
   Avatar,
-  ChakraComponent,
+  ChakraComponent
 } from '@chakra-ui/react';
 import { CardData } from '@/pages/api/get-cards';
-import Link from 'next/link'
+import Link from 'next/link';
+import React from 'react';
 
-const CardDisplayer: ChakraComponent<ChakraComponent<"div", {}>> = ({ cards }) => {
-  return cards.map((c: CardData, idx: Number) => {
-    const mainImage = c.mainImage.replace('/public', '');
-    const lastCardIndex = cards.length - 1;
+const CardDisplayer: React.FC<{ cards: CardData[] }> = ({ cards }) => {
+  const getImage = (c: CardData) => {
+    return c.mainImage.replace('/public', '');
+  };
+  const lastCardIndex = cards.length - 1;
 
-    return (
-      <Box key={c.id}>
-        <Card variant="unstyled" w="100%" px="25px" mb="25px">
-          <CardBody>
-            <Box pb="17px">
-              <Link href={`/card/${c.id}`}>
-                <Image
-                  src={mainImage}
-                  alt="card"
-                  width="100%"
-                  height="100%"
-                />
-              </Link>
-            </Box>
+  return (
+    <>
+      {cards.map((c: CardData, idx: Number) => (<Box key={c.id}>
+          <Card variant='unstyled' w='100%' px='25px' mb='25px'>
+            <CardBody>
+              <Box pb='17px'>
+                <Link href={`/card/${c.id}`}>
+                  <Image
+                    src={getImage(c)}
+                    alt='card'
+                    width='100%'
+                    height='100%'
+                  />
+                </Link>
+              </Box>
 
-            <VStack spacing="4" align="start">
-              <HStack spacing="2">
-                <Avatar src={mainImage} w="38px" h="38px" />
-                <Text>@{c.nickname}</Text>
-              </HStack>
+              <VStack spacing='4' align='start'>
+                <HStack spacing='2'>
+                  <Avatar src={getImage(c)} w='38px' h='38px' />
+                  <Text>@{c.nickname}</Text>
+                </HStack>
 
-              <Divider borderBottom="bdBottom" />
+                <Divider borderBottom='bdBottom' />
 
-              <Heading fontWeight="medium" fontSize="fs4" lineHeight="lh1">
-                {c.title}
-              </Heading>
+                <Heading fontWeight='medium' fontSize='fs4' lineHeight='lh1'>
+                  {c.title}
+                </Heading>
 
-              <Text>{c.description}</Text>
+                <Text>{c.description}</Text>
 
-              <Heading fontSize="fs5" lineHeight="lh2" fontWeight="medium">
-                {`USD ${c.price}`}
-              </Heading>
-            </VStack>
-          </CardBody>
-        </Card>
+                <Heading fontSize='fs5' lineHeight='lh2' fontWeight='medium'>
+                  {`USD ${c.price}`}
+                </Heading>
+              </VStack>
+            </CardBody>
+          </Card>
 
-        {idx === lastCardIndex ? (
-          <Divider mt="77px" mb="25px" borderBottom="bdBottom" />
-        ) : (
-          <Divider mt="22px" mb="25px" borderBottom="bdBottom" />
-        )}
-      </Box>
-    );
-  });
+          {idx === lastCardIndex ? (
+            <Divider mt='77px' mb='25px' borderBottom='bdBottom' />
+          ) : (
+            <Divider mt='22px' mb='25px' borderBottom='bdBottom' />
+          )}
+        </Box>
+      ))}
+    </>
+  );
 };
 
 export default CardDisplayer;
